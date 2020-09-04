1b1ba9f8b4c053aedf07989a667154f3a386e7dc
Aradhya Mehta <aradhyamehta1@gmail.com>
2020-09-04 11:04:29 +0530

    Landing page almost ready. Not responsive yet

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from "./sample.jpg";
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+
+function shoot() {
+  alert("Great Shot!");
+}
 
 function App() {
   return (
     <div className="App">
       <div className="TopNav">
         <h1 className="h1-style">Welcome to BugFree
-          <FontAwesomeIcon className="icon-style" icon={faCodeBranch} spin />
+          <a className="click-style" href="https://github.com/aradhyamehta/issue-tracker.git"><FontAwesomeIcon className="icon-style" icon={faCodeBranch} spin /></a>
         </h1>
       </div>
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button
-            className="Button-style"
-          >
-            LOGIN
-          </button>
+        <div className="LeftBody">
+      <h3>BugFree is a software development tool to view & store all your programming issues.</h3>
+      <h3>BugFree is a software development tool to view & store all your programming issues.</h3>
+      <button className="button-style" onClick={shoot}>ENTER HERE</button>
+      </div>
+      <div className="RightBody">
+        <img src={image} className="App-logo" alt="logo" />
+      </div>
       </header>
     </div>
   );
