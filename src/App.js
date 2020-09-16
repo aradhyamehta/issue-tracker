feee2642f6d662a23480c60f160f9e320d0b8ff6
Aradhya Mehta <aradhyamehta1@gmail.com>
2020-09-16 23:35:32 +0530

    dashed button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Button, DatePicker } from 'antd';
+import { Button, DatePicker, Input } from 'antd';
 import 'antd/dist/antd.css';
 
 function App() {
@@ -10,6 +10,8 @@ function App() {
     <div>
     <Button type="primary">PRESS ME</Button>
     <DatePicker placeholder="select date" />
+    <Input placeholder="Basic usage" />
+    <Button type="dashed">Dashed Button</Button>
       </div>
   );
 }
