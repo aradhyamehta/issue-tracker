4d2b5ad36c080a2da332227bbfefe6c3b40460a7
Aradhya Mehta <aradhyamehta1@gmail.com>
2020-09-16 23:26:55 +0530

    removed content

--- src/App.js
+++ src/App.js
@@ -11,19 +11,9 @@ function App() {
   return (
 
     <div>
-    <Layout className="layout">
-    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">
-      <Button type="primary">PRESS ME</Button>
+    <Button type="primary">PRESS ME</Button>
     <DatePicker placeholder="select date" />
-    <Text code>BugFree is a software development tool to view & store all your programming issues.</Text>
-      </div>
-    </Content>
+    <Layout className="layout">
     <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
   </Layout>
       </div>
